ce8736307e3aa34a91c85d04492073e5fb242947
Marek Výmola <vymola_marek@oauh.cz>
2024-10-07 10:19:34 +0200

    funkcni editor

---
+++ resources/views/components/head/tinymce-config.blade.php
@@ -0,0 +1,8 @@
+<script src="https://cdn.tiny.cloud/1/loi29jlj2bf1ukbj9xb5j7p6akxy11k9528qlz0tbamhpu5z/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
+<script>
+  tinymce.init({
+    selector: 'textarea#myeditorinstance', // Replace this CSS selector to match the placeholder element for TinyMCE
+    plugins: 'code table lists',
+    toolbar: 'undo redo | blocks | bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table'
+  });
+</script>
--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -4,24 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WYSIWYG Editor</title>
-    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
-    <script>
-        tinymce.init({
-            selector: '#editor',
-            plugins: 'lists link image table code',
-            toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent | link image | code',
-        });
-    </script>
+    <x-head.tinymce-config/>
+
 </head>
 <body>
     <h1>WYSIWYG Editor</h1>
-    @if(session('success'))
-        <div style="color: green;">{{ session('success') }}</div>
-    @endif
-    <form action="{{ route('editor.save') }}" method="POST">
-        @csrf
-        <textarea id="editor" name="content"></textarea>
-        <button type="submit">Save</button>
-    </form>
+    <x-forms.tinymce-editor/>
+
 </body>
 </html>
